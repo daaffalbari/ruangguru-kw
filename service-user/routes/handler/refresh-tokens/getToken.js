990952284bb4473022b88ddd0b11882f4807063d
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2022-09-20 12:16:47 +0700

    integrasi api register

--- service-user/routes/handler/refresh-tokens/getToken.js
+++ service-user/routes/handler/refresh-tokens/getToken.js
@@ -1,12 +1,15 @@
+// Import Refresh Token Model
 const { RefreshToken } = require('../../../models');
 
 module.exports = async (req, res) => {
+  // Get Refresh Token
   const refreshToken = req.query.refresh_token;
 
   const token = await RefreshToken.findOne({
     where: { token: refreshToken },
   });
 
+  // Check Refresh Token Exist or Not
   if (!token) {
     return res.status(404).json({
       status: 'error',
